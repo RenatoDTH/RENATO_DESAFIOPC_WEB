7d3a2cdf0a25470b8e27435e1a9fa43bcee1c029
Renato Castro <renatocastro02@hotmail.com>
2021-02-04 17:07:22 -0300

    feat: Start the building of the page placing and styling the components

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,54 @@
-import { Container } from './styles';
+import {
+  Container,
+  ContainerText,
+  Content,
+  ContentHeader,
+  ContentBody,
+  FormWindow,
+} from './styles';
 import Header from '../../components/Header';
+import Input from '../../components/Input';
+import Textarea from '../../components/Textarea';
+import pc from '../../assets/photo.svg';
 
 const Home = () => {
   return (
     <Container>
       <Header />
-      <p>Hello Project</p>
+      <ContainerText>
+        <h1>RECEBA NOSSO MATERIAL GRATUITAMENTE!</h1>
+      </ContainerText>
+      <Content>
+        <ContentHeader>
+          <h1>Descubra como fazer um curso pode mudar sua vida!</h1>
+          <h2>Preencha os campos abaixo para receber</h2>
+        </ContentHeader>
+        <ContentBody>
+          <img src={pc} alt="pc" />
+          <FormWindow>
+            <Input
+              name="nome"
+              label="Nome"
+              type="text"
+              placeholder="Colocar o nome"
+            />
+            <Input
+              name="email"
+              label="Email"
+              type="email"
+              placeholder="Colocar o E-mail"
+            />
+            <Input
+              name="telefone"
+              label="Telefone"
+              type="tel"
+              placeholder="DDD-00000-0000"
+              pattern="[0-9]{3}-[0-9]{5}-[0-9]{4}"
+            />
+            <Textarea name="textarea" type="text" />
+          </FormWindow>
+        </ContentBody>
+      </Content>
     </Container>
   );
 };
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -4,4 +4,57 @@ export const Container = styled.div`
   height: 100vh;
   display: flex;
   background: #07305a;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const ContainerText = styled.div`
+  margin: 40px auto;
+  h1 {
+    color: #fff;
+    font-weight: 700;
+    line-height: 72px;
+  }
+`;
+
+export const Content = styled.div`
+  margin: 0px 100px 100px 50px;
+  padding: 20px 10px;
+  width: 100%;
+  max-width: 1200px;
+  background: #eaf0f4;
+  border-radius: 30px;
+  border: 1px solid rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-items: center;
+  flex-direction: column;
+`;
+
+export const ContentHeader = styled.div`
+  h1 {
+    font-weight: 700;
+    size: 32px;
+    line-height: 36px;
+    color: #000000;
+  }
+  h2 {
+    size: 22px;
+    font-weight: 400;
+    line-height: 36px;
+    letter-spacing: -0.015em;
+  }
+`;
+
+export const ContentBody = styled.div`
+  margin-top: 50px;
+  display: flex;
+
+  img {
+    width: 500px;
+    height: 417px;
+  }
+`;
+
+export const FormWindow = styled.div`
+  flex-direction: column;
 `;
